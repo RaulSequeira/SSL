d0f40c7ded16172a9f78606ee326ac2678e59292
RaulSequeira <raulsequeira@frba.utn.edu.ar>
2023-04-25 17:48:22 -0300

    Se escribe el Hello, World! en un txt

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -2,7 +2,15 @@
 
 #include <stdio.h>
 int main() {
-    // printf() displays the string inside quotation
-    printf("Hello, World!");
+    char *nombreArchivo = "output.txt";
+    char *modo = "w";// w es para sobrescribir, a+ es para añadir al existente
+    FILE *archivo = fopen(nombreArchivo, modo);
+    // Si por alguna razón el archivo no fue abierto, salimos inmediatamente
+    if (archivo == NULL) {
+        printf("Error abriendo archivo %s", nombreArchivo);
+        return 1;
+    }
+
+    fprintf(archivo, "Hello, World!\n");
     return 0;
 }
